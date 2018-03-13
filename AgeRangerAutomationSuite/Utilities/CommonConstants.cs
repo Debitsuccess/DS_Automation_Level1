9418bac2023a0af007282abda9902ea73504f3f4
ADFITAUCK\achyuthan.k <achyuthan.k@debitsuccess.com>
2018-03-14 11:40:53 +1300

    Creating the repository

---
+++ AgeRangerAutomationSuite/Utilities/CommonConstants.cs
@@ -0,0 +1,23 @@
+﻿namespace AgeRangerWebUi.Utilities
+{
+    public static class CommonConstants
+    {
+        public static class DriverSettings
+        {
+            public static string BinaryLocation = @"C:\Users\ramya.j\source\repos\DS_Automation_PlayGround\Level1\AgeRangerAutomationSuite\Drivers";
+
+            public static string FireFoxBrowser = "FireFox";
+            public static string ChromeBrowser = "Chrome";
+            public static string IEBrowser = "IE";
+
+            public static string WindowsPlatform = "Windows";
+
+            public static int DefaultWaitTime = 3000;
+        }
+
+        public static class ApplicationSettings
+        {
+            public static string BaseUrl = "http://ageranger.azurewebsites.net";
+        }
+    }
+}
